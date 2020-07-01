5fdde1569698fd2001abc59dd8aee0b5a1e244d9
angelynaliem <angelynaliem@gmail.com>
2020-06-30 23:42:05 -0400

    Added append and prepend

--- js/index.js
+++ js/index.js
@@ -41,24 +41,39 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-let navItems = document.querySelectorAll("a");
 
-let navItem1 = navItems[0].textContent = siteContent["nav"]["nav-item-1"];
-let navItem2 = navItems[1].textContent = siteContent["nav"]["nav-item-2"];
-let navItem3 = navItems[2].textContent = siteContent["nav"]["nav-item-3"];
-let navItem4 = navItems[3].textContent = siteContent["nav"]["nav-item-4"];
-let navItem5 = navItems[4].textContent = siteContent["nav"]["nav-item-5"];
-let navItem6 = navItems[5].textContent = siteContent["nav"]["nav-item-6"];
+// Get the nav items to show up. WHY DO WE NEED TO PUT INDEX 0 HERE?
+let navItems = document.querySelector("nav");
 
+let navItem1 = navItems.children[0].textContent = siteContent["nav"]["nav-item-1"];
+let navItem2 = navItems.children[1].textContent = siteContent["nav"]["nav-item-2"];
+let navItem3 = navItems.children[2].textContent = siteContent["nav"]["nav-item-3"];
+let navItem4 = navItems.children[3].textContent = siteContent["nav"]["nav-item-4"];
+let navItem5 = navItems.children[4].textContent = siteContent["nav"]["nav-item-5"];
+let navItem6 = navItems.children[5].textContent = siteContent["nav"]["nav-item-6"];
+
+// Add two new nav items using append and prepend
+let newNavItem1 = document.createElement("a");
+newNavItem1.textContent = "Sign Up";
+navItems.appendChild(newNavItem1);
+
+let newNavItem2 = document.createElement("a");
+newNavItem2.textContent = "Log In";
+navItems.prepend(newNavItem2);
+
+// Get h1 text to show up
 let h1 = document.querySelector("h1");
 h1.textContent = siteContent["cta"]["h1"];
 
+// Get button content to show up
 let button = document.querySelector("button");
 button.textContent = siteContent["cta"]["button"];
 
+// Get the round image to show up
 let imgSrc = document.getElementById("cta-img");
 imgSrc.setAttribute('src', siteContent["cta"]["img-src"]);
 
+// Get all the h4 content to show up
 let h4s = document.querySelectorAll("h4");
 
 let featuresh4 = h4s[0].textContent = siteContent["main-content"]["features-h4"];
@@ -68,9 +83,11 @@ let producth4 = h4s[3].textContent = siteContent["main-content"]["product-h4"];
 let visionh4 = h4s[4].textContent = siteContent["main-content"]["vision-h4"];
 let contacth4 = h4s[5].textContent = siteContent["contact"]["contact-h4"];
 
+// Get the middle image to show up
 let middleImgSrc = document.getElementById("middle-img");
 middleImgSrc.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
+// Get all the p content to show up
 let ps = document.querySelectorAll("p");
 
 let featuresContent = ps[0].textContent = siteContent["main-content"]["features-content"];
@@ -83,17 +100,15 @@ let phone = ps[6].textContent = siteContent["contact"]["phone"];
 let email = ps[7].textContent = siteContent["contact"]["email"];
 let copyright = ps[8].textContent = siteContent["footer"]["copyright"];
 
-navItems[0].style.color = 'green';
-navItems[1].style.color = 'green';
-navItems[2].style.color = 'green';
-navItems[3].style.color = 'green';
-navItems[4].style.color = 'green';
-navItems[5].style.color = 'green';
-navItems[6].style.color = 'green';
-
-
-
-
+// Change the nav items color to green
+navItems.children[0].style.color = 'green';
+navItems.children[1].style.color = 'green';
+navItems.children[2].style.color = 'green';
+navItems.children[3].style.color = 'green';
+navItems.children[4].style.color = 'green';
+navItems.children[5].style.color = 'green';
+navItems.children[6].style.color = 'green';
+newNavItem1.style.color = "green";
 
 
 
